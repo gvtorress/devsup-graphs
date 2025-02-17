3782b7936f8c4203651dfb3efa2f8d120d637b44
Gabriel Torres <gvtorress@gmail.com>
2025-02-17 14:33:40 -0300

    implement connectedComponents function

--- bfs.js
+++ bfs.js
@@ -13,32 +13,56 @@ class Graph {
     return this.adjList[v];
   }
 
-  bfs = (v) => {
-    let visited = Array(this.numVertices).fill(false);
+  bfs = (v, visited = null) => {
+    let visitedArr = visited ? visited : Array(this.numVertices).fill(false);
     let queue = [];
 
     queue.push(v);
-    visited[v] = true;
+    visitedArr[v] = true;
 
     while (queue.length > 0) {
       v = queue.shift();
       console.log(`Visited ${v}`);
 
       for (const w of this.neighbours(v)) {
-        if (!visited[w]) {
-          visited[w] = true;
+        if (!visitedArr[w]) {
+          visitedArr[w] = true;
           queue.push(w);
         }
       }
     }
   }
+
+  connectedComponents = () => {
+    let visited = Array(this.numVertices).fill(false);
+    let cont = 0;
+
+    for (let i = 0; i < this.numVertices; i++) {
+      if (!visited[i]) {
+        console.log(`Component ${cont}`);
+        this.bfs(i, visited);
+        cont++;
+      }
+    }
+
+    return cont;
+  }
 }
 
-const graph = new Graph(5);
-graph.addEdge(0, 1);
-graph.addEdge(0, 2);
-graph.addEdge(0, 3);
-graph.addEdge(2, 3);
-graph.addEdge(2, 4);
-graph.addEdge(3, 4);
-graph.bfs(0);
+const graph1 = new Graph(5);
+graph1.addEdge(0, 1);
+graph1.addEdge(0, 2);
+graph1.addEdge(0, 3);
+graph1.addEdge(2, 3);
+graph1.addEdge(2, 4);
+graph1.addEdge(3, 4);
+graph1.bfs(0);
+
+const graph2 = new Graph(7);
+graph2.addEdge(0, 1);
+graph2.addEdge(0, 2);
+graph2.addEdge(1, 2);
+graph2.addEdge(3, 4);
+graph2.addEdge(2, 6);
+
+console.log(graph2.connectedComponents());
